a4926483b0f09d72f35dd13d1a189769328e0c49
Thota <jthota@mcw.edu>
2026-08-04 10:25:05 -0500

    fixed typo

--- src/main/java/edu/mcw/scge/platform/Main.java
+++ src/main/java/edu/mcw/scge/platform/Main.java
@@ -139,11 +139,11 @@ public class Main {
 
         String baseURI="https://clinicaltrials.gov/api/v2/studies?countTotal=true" +
                 "&query.term=" +
-                "AREA[protocolSection.descriptionModule.briefSummary](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crisper OR crisper ) OR " +
-                "AREA[protocolSection.descriptionModule.detailedDescription](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crisper OR crisper) OR " +
-                "AREA[protocolSection.identificationModule.officialTitle](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crisper OR crisper) OR " +
-                "AREA[InterventionSearch](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crisper OR crisper) OR " +
-                "AREA[ConditionSearch](gene therapy OR gene transfer OR gene editing OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crisper OR crisper)" +
+                "AREA[protocolSection.descriptionModule.briefSummary](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crispr OR crispr ) OR " +
+                "AREA[protocolSection.descriptionModule.detailedDescription](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crispr OR crispr) OR " +
+                "AREA[protocolSection.identificationModule.officialTitle](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crispr OR crispr) OR " +
+                "AREA[InterventionSearch](gene therapy OR gene transfer OR gene editing OR CRISPR OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crispr OR crispr) OR " +
+                "AREA[ConditionSearch](gene therapy OR gene transfer OR gene editing OR Gene Therapy OR Gene Transfer OR Gene Editing OR Crispr OR crispr)" +
                 "&postFilter.advanced=AREA[protocolSection.oversightModule.isFdaRegulatedDrug]true"+
                 "&filter.advanced=AREA[LastUpdatePostDate]RANGE[2023-01-01, MAX]";
 
